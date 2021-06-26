8865c4d5a7cca7ef33451fdca3c13055c6da1c6e
Kafayat Jimoh <olajumokekifaayah@gmail.com>
2021-06-26 10:00:03 +0100

    my javascript ptactice

---
+++ code.js
@@ -0,0 +1,24 @@
+/*let name = "Ada";
+alert(name);
+
+var colours = ["red", "blue", "green", "yellow"];
+console.log(colours);
+
+var info = ["olajumoke", "Female", "Islam", "Yoruba"];
+console.log(info);
+
+let firstName = "Kifaayah";
+let lastName = "Jimoh";
+let middleName = "Olajumoke"
+let fullName = lastName + " " + firstName + " " + middleName ;
+console.log(fullName);
+*/
+
+let message = prompt(" What is your name dear customer?");
+let tag = prompt(`Welcome to our store ${message}`);
+let amount = prompt(`How much is the item bought ${message}`);
+let reply = (`Dear ${message} the price of items you bought from our store is ${amount} dollar?`);
+console.log(reply);
+
+// let price = prompt("How much is the price of the good you bought?");
+// console.log(`The price of the item is: ${price} dollar`);
